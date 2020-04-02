5c3ea15b3b44e66afb2870e32a7999dd4dd702f9
Chris Evans <chrisevans9629@gmail.com>
2020-04-01 21:40:02 -0500

    gripping works

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Drag : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.instance.leftHand.grabGripAction.state)
+        {
+            Debug.Log("Left Grip!!");
+        }
+        if (Player.instance.rightHand.grabGripAction.state)
+        {
+            Debug.Log("Left Grip!!");
+        }
+    }
+}

6db6c25e4b86087ecc08bf21169d47431ad3b805
Chris Evans <chrisevans9629@gmail.com>
2020-04-02 17:15:51 -0500

    grabbing objects works

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -2,26 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
-
+[RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Rigidbody))]
 public class Drag : MonoBehaviour
 {
-    
+    public CharacterJoint JointPrefab;
+    private Interactable interactable;
+
+    private Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        interactable = GetComponent<Interactable>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+   // private Vector3 oldPosition;
+    //private Quaternion oldRotation;
+    //private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+    private void HandHoverUpdate(Hand hand)
     {
-        if (Player.instance.leftHand.grabGripAction.state)
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
         {
-            Debug.Log("Left Grip!!");
+            // Save our position/rotation so that we can restore it when we detach
+            //oldPosition = transform.position;
+            //oldRotation = transform.rotation;
+
+            // Call this to continue receiving HandHoverUpdate messages,
+            // and prevent the hand from hovering over anything else
+           // hand.HoverLock(interactable);
+
+            var joint = hand.GetComponentInChildren<CharacterJoint>();
+            if (joint == null)
+            {
+                joint = Instantiate(JointPrefab, hand.transform.position, hand.transform.rotation, hand.transform);
+                joint.connectedBody = rigidbody;
+            }
+
+
+            //joint.connectedBody =
+            // Attach this object to the hand
+            //hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
         }
-        if (Player.instance.rightHand.grabGripAction.state)
+        else if (isGrabEnding)
         {
-            Debug.Log("Left Grip!!");
+            // Detach this object from the hand
+            //hand.DetachObject(gameObject);
+
+            // Call this to undo HoverLock
+            //hand.HoverUnlock(interactable);
+            var joint = hand.gameObject.GetComponentInChildren<CharacterJoint>();
+            if (joint != null)
+            {
+                Destroy(joint.gameObject);
+            }
+            // Restore position/rotation
+            //transform.position = oldPosition;
+            //transform.rotation = oldRotation;
         }
     }
 }

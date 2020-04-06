b74784b121c5101b718d21efc45c9405ae463910
Chris Evans <chrisevans9629@gmail.com>
2020-04-06 18:09:47 -0500

    game restart works

--- Assets/Scripts/GenerateTower.cs
+++ Assets/Scripts/GenerateTower.cs
@@ -17,40 +17,37 @@ public class GenerateTower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BuildTower();
+    }
+
+    public void BuildTower()
+    {
         for (int floor = 0; floor < TowerFloors; floor++)
         {
-            
             for (int plank = -1; plank < 2; plank++)
             {
+                var box = CreatePlank();
+
                 if (floor % 2 != 0)
                 {
-                    var box = Instantiate(Prefab, transform.position, Quaternion.identity, transform);
-
-                    var drags = box.GetComponents<Drag>();
-                    drags[0].Hand = leftHand;
-                    drags[1].Hand = rightHand;
-
                     box.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
                     box.transform.localPosition = new Vector3(plank * Spacing.x, floor * Spacing.y, 0);
                 }
                 else
                 {
-                    var box = Instantiate(Prefab, transform.position, Quaternion.identity, transform);
-                    // box.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
-
-                    var drags = box.GetComponents<Drag>();
-                    drags[0].Hand = leftHand;
-                    drags[1].Hand = rightHand;
                     box.transform.localPosition = new Vector3(0, floor * Spacing.y, plank * Spacing.z);
                 }
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject CreatePlank()
     {
-
-       
+        var box = Instantiate(Prefab, transform.position, Quaternion.identity, transform);
+        var drags = box.GetComponents<Drag>();
+        drags[0].Hand = leftHand;
+        drags[1].Hand = rightHand;
+        return box;
     }
+
 }
---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    private GenerateTower tower;
+    void Start()
+    {
+        tower = GameObject.FindObjectOfType<GenerateTower>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plank"))
+        {
+            foreach (Transform o in tower.transform)
+            {
+                Destroy(o.gameObject);
+            }
+
+            tower.BuildTower();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+
+
+}

d96d9706d6119ae92723859d8400b87fa149d3fe
Chris Evans <chrisevans9629@gmail.com>
2020-04-08 17:51:31 -0500

    added toon shader

---
+++ Assets/Scripts/SoundCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollision : MonoBehaviour
+{
+
+    public float SoundRatio;
+    public float Max;
+    public float Min;
+    public AudioSource Audio;
+
+   
+}
